1b17dafacb7cece54f3e6e8e4710b3764701b5a5
ZEKN <ferrarizekn@gmail.com>
2013-12-06 09:38:15 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Postwall::Application.config.secret_key_base = '4d47d30aa983ceaa2eed2cbb462a000ecf211ecdfb875b6e5e1be2d42a384753098760b0fb515fe5214ae74603a1d45daa52ed1c975d937ff936ec2eb47942ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Postwall::Application.config.session_store :cookie_store, key: '_postwall_session'
